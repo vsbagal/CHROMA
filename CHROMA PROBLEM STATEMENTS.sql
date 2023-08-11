d089c0a94d29cad364ad08fd6d02886e8d98fcb8
Vishal Bagal <125578339+vsbagal@users.noreply.github.com>
2023-08-11 16:15:26 +0530

    Add files via upload

---
+++ CHROMA PROBLEM STATEMENTS.sql
@@ -0,0 +1,50 @@
+-- PROBLEM STATEMENTS 
+-- 1. Sales Analysis:
+   -- Calculate the total sales and revenue for a specific date range.
+   -- Identify the top-selling products in terms of revenue and quantity sold.
+   -- Determine the most popular product categories based on sales.
+
+-- 2. Inventory Management:
+   -- Generate a report of products with low stock quantities to prioritize restocking.
+   -- Calculate the total value of the current inventory.
+   -- Identify products with zero sales over a certain period to consider discontinuation.
+
+/* 3. Customer Analysis:
+   - Find the total number of customers who made purchases within a given time frame.
+   - Identify the highest-spending customers based on their total purchase amount.
+   - Analyze customer demographics based on location and purchasing behavior. */
+
+ /* 4. Order Insights:
+   - Calculate the average order value and total number of orders per month.
+   - Identify patterns in order frequency and size for different customer segments.
+   - Determine the average time between order placement and fulfillment. */
+
+-- 5. Product Performance: 
+   -- Compare the sales performance of products from different brands and categories.
+   -- Analyze the impact of product price changes on sales volume and revenue.
+   -- Calculate the average price and quantity of products sold per order.
+
+-- 6. Customer Loyalty:
+   -- Calculate the repeat purchase rate for customers who made multiple orders.
+   -- Identify customers who haven't made a purchase in a certain period for targeted marketing.
+   -- Analyze the relationship between customer loyalty and purchase frequency.
+
+-- 7. Promotions and Discounts:
+   -- Evaluate the effectiveness of promotional campaigns on sales and customer engagement.
+   -- Calculate the percentage of revenue generated from discounted products.
+   -- Identify the most successful types of promotions based on customer response.
+
+-- 8. Geographic Insights:
+   -- Visualize sales data on a map to identify regions with the highest and lowest sales.
+   -- Analyze whether customer demographics impact purchasing behavior across different locations.
+   -- Calculate the average order value for customers from different regions.
+
+-- 9. Order Fulfillment:
+   -- Analyze order processing times and identify bottlenecks in the fulfillment process.
+   -- Calculate the percentage of orders fulfilled on time and late deliveries.
+   -- Identify the average time taken to fulfill orders for different product categories.
+
+-- 10. Product Bundling:
+    -- Identify frequently co-purchased products to create product bundling recommendations.
+    -- Analyze the impact of offering product bundles on overall sales and revenue.
+    -- Calculate the average discount percentage for products included in bundles.
